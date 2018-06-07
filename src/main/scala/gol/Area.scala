9230dc06a76d8f99ad43b837b40a7dadc08a1e69
Domonkos Suranyi <suranyi.domi@gmail.com>
2018-06-07 16:19:56 +0200

    Implement method that returns with a 3x3 subarray of the given big array
    The given cell and its neighbours

--- src/main/scala/gol/Area.scala
+++ src/main/scala/gol/Area.scala
@@ -1,7 +1,24 @@
 package gol
 
 class Area (val cells: Array[Array[Cell]]) {
-        
+    private def getCellWithNeighbours(coord: (Int, Int)) = {
+        var retArray = Array.ofDim[Cell](3, 3);
+        for (x <- -1 to 1) {
+            for (y <- -1 to 1) {
+                val newCoord = (coord._1+x, coord._2+y);
+                retArray(x+1)(y+1) = newCoord match {
+                    case (-1, _) => Cell.DEAD
+                    case (_, -1) => Cell.DEAD
+                    case (size, _) => Cell.DEAD
+                    case (_, size) => Cell.DEAD
+                    case _ => cells(newCoord._1)(newCoord._2)
+                }
+            }
+        }
+        retArray
+    }
+
+    def size: Int = cells.size;
 }
 
 object Area {
