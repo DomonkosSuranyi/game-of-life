fb6f2b6bba8a2a03483b01a492e7245559badb43
Domonkos Suranyi <suranyi.domi@gmail.com>
2018-06-11 08:50:20 +0200

    Retab
    using 2-space style instead of 4-space style

--- src/main/scala/gol/Area.scala
+++ src/main/scala/gol/Area.scala
@@ -1,33 +1,33 @@
 package gol
 
 class Area (val cells: Array[Array[Cell]]) {
-    private def getCellWithNeighbours(coord: (Int, Int)) = {
-        var retArray = Array.ofDim[Cell](3, 3);
-        for (x <- -1 to 1) {
-            for (y <- -1 to 1) {
-                val newCoord = (coord._1+x, coord._2+y);
-                retArray(x+1)(y+1) = newCoord match {
-                    case (-1, _) => Cell.DEAD
-                    case (_, -1) => Cell.DEAD
-                    case (size, _) => Cell.DEAD
-                    case (_, size) => Cell.DEAD
-                    case _ => cells(newCoord._1)(newCoord._2)
-                }
-            }
-        }
-        retArray
-    }
+  def size: Int = cells.size;
 
-    def size: Int = cells.size;
+  private def getCellWithNeighbours(coord: (Int, Int)) = {
+    var retArray = Array.ofDim[Cell](3, 3);
+    for (x <- -1 to 1) {
+      for (y <- -1 to 1) {
+        val newCoord = (coord._1+x, coord._2+y);
+        retArray(x+1)(y+1) = newCoord match {
+          case (-1, _) => Cell.DEAD
+          case (_, -1) => Cell.DEAD
+          case (size, _) => Cell.DEAD
+          case (_, size) => Cell.DEAD
+          case _ => cells(newCoord._1)(newCoord._2)
+        }
+      }
+    }
+    retArray
+  }
 }
 
 object Area {
-    def apply(percent: Int, size: Int) = {
-        new Area(generateRandom2DArray(size, percent));
-    }
+  def apply(percent: Int, size: Int) = {
+    new Area(generateRandom2DArray(size, percent));
+  }
 
-    private def generateRandom2DArray(size: Int, percent: Int) = {
-        Array.fill(2)(Array.fill(size)(Cell.randomCell(percent)));
-    }
+  private def generateRandom2DArray(size: Int, percent: Int) = {
+    Array.fill(2)(Array.fill(size)(Cell.randomCell(percent)));
+  }
 }
 
--- src/main/scala/gol/Cell.scala
+++ src/main/scala/gol/Cell.scala
@@ -5,13 +5,14 @@ import scala.util.Random
 sealed abstract class Cell;
 
 object Cell {
-    case object LIVING extends Cell;
-    case object DEAD   extends Cell;
+  case object LIVING extends Cell;
+  case object DEAD   extends Cell;
 
-    def randomCell(probabOfLiving: Int) : Cell = {
-        if (probabOfLiving > 100 || probabOfLiving < 1)
-            throw new IllegalArgumentException("Invalid value as percent (" + probabOfLiving + "%)");
-        if (Random.nextInt(100) >= probabOfLiving) { DEAD } else { LIVING }
-    }
+  def randomCell(probabOfLiving: Int) : Cell = {
+    if (probabOfLiving > 100 || probabOfLiving < 1)
+      throw new IllegalArgumentException("Invalid value as percent (" + probabOfLiving + "%)");
+    if (Random.nextInt(100) >= probabOfLiving) { DEAD } else { LIVING }
+  }
 }
+
 
